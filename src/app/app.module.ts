82956733e38929e6746be3bc1f9ee32317c4f86b
Walid HAMMAMI <walid.ham@hotmail.com>
2023-12-10 11:10:38 +0100

    ngModel, FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactItemComponent } from './contact-item/contact-item.component';
 import { ContactAddComponent } from './contact-add/contact-add.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [RootComponent]
--- src/app/contact-add/contact-add.component.ts
+++ src/app/contact-add/contact-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../models/contact.model';
 
 @Component({
   selector: 'app-contact-add',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactAddComponent implements OnInit {
 
+  contact : Contact = new Contact(0,"","","","Work","","");
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  onSubmit(){
+    
+  }
 
+  addContact(){
+    alert("Contact added");
+    
+  }
 }
