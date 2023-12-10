9c8f7f822467983db82063711b188b2cdd230c47
Walid HAMMAMI <walid.ham@hotmail.com>
2023-12-10 10:33:41 +0100

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { RootComponent } from './root/root.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactItemComponent } from './contact-item/contact-item.component';
+import { ContactAddComponent } from './contact-add/contact-add.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     RootComponent,
     MenuComponent,
     ContactListComponent,
-    ContactItemComponent
+    ContactItemComponent,
+    ContactAddComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
